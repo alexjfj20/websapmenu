57f8559887232551a69cdefc5b999acba1cf1752
Alexander Jerez Fernández <92186808+alexjfj20@users.noreply.github.com>
2025-05-20 11:41:05 -0500

    Update authService.js

--- authService.js
+++ authService.js
@@ -1,2 +1,3 @@
 console.log("Usuario buscado:", email);
 console.log("Lista de usuarios:", usuarios.map(u => u.email)); 
+Enviando solicitud al backend: https://websapmenu.onrender.com/api/auth/login
